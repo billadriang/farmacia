80e7b60759f3d87560bbcf2a3b1c5fd0bdf46b5e
billadriang <billadriang@gmail.com>
2022-06-09 22:44:29 -0400

    primeros pasos en react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Tomate.svg/267px-Tomate.svg.png" className="App-logo" alt="tomate" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          UN TOMATE POR MIENTRAS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/billadriang"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ve mi Github plz
         </a>
       </header>
     </div>
